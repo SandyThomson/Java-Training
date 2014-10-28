34d717ce72f3f0993931f2f2c4f1611bbbb8cbc2
Michael Wambeek <msswambeek@gmail.com>
2014-10-28 20:54:48 +0000

    adding gallon to litres example

---
+++ src/main/java/GalToLit.java
@@ -0,0 +1,20 @@
+/**
+ * @author Mike
+ *
+ */
+class GalToLit {
+	/**
+	 * @param args
+	 */
+	public static void main(String args[]) {
+
+		double gallons;
+		double litres;
+
+		gallons = 10;
+
+		litres = gallons * 3.7854;
+
+		System.out.println(gallons + " gallons is " + litres + " litres.");
+	}
+}
