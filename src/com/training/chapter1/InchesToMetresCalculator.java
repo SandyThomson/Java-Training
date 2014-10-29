a7020d1866f3bd92ca73df90813f75ad89f75a25
Simon Gross <spg@datacash.com>
2014-10-29 16:20:53 +0000

    Formatting fix

--- src/com/training/chapter1/InchesToMetresCalculator.java
+++ src/com/training/chapter1/InchesToMetresCalculator.java
@@ -1,22 +1,22 @@
+
 package com.training.chapter1;
 
 import java.util.function.IntConsumer;
-
 
 public class InchesToMetresCalculator implements IntConsumer {
 
 	// The ratio of inches per metre
 	private static final double INCHES_TO_METRES_RATIO = 39.37;
-	
+
 	@Override
-  public void accept( int arg0 ) {
-	
+	public void accept( int arg0 ) {
+
 		System.out.println( arg0 + " inches is " + arg0 * INCHES_TO_METRES_RATIO + " metres" ); // Output result
 
 		if ( arg0 % 12 == 0 ) {
 			System.out.println(); // Output blank line every 10 lines
 		}
 
-  }
+	}
 
 }
