28f88c74aa262d22523eac6901f76d26d3120e55
gxd <graham.davidson@datacash.com>
2014-11-13 15:47:30 +0000

    Chapter 3 answers

---
+++ src/question2/IfElseLadder.java
@@ -0,0 +1,11 @@
+package question2;
+
+public class IfElseLadder {
+	public static void main( String[] args ) {
+		int a = 1, b = 2;
+
+		if      ( a > b ) System.out.println( "a is greater than b" );
+		else if ( a < b )	System.out.println( "a is less than b"    );
+		else         			System.out.println( "a is equal to b"     );
+	}
+}
