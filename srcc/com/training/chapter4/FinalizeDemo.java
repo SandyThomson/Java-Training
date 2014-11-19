e8e6d30e41c9ad6bf86db2459d11190d6a63da92
Simon Gross <spg@datacash.com>
2014-11-19 09:10:51 +0000

    Formatting

--- srcc/com/training/chapter4/FinalizeDemo.java
+++ srcc/com/training/chapter4/FinalizeDemo.java
@@ -1,17 +1,18 @@
+
 package com.training.chapter4;
 
 public class FinalizeDemo {
 
 	private static final int max = 1000000;
 
-	public static void main(String[] args) {
-		System.out.println("Let's be dustmen!");
+	public static void main( String[] args ) {
+		System.out.println( "Let's be dustmen!" );
 
-		for (int count = 0; count < max; count++) {
-			Finalizer.generate(count);
+		for ( int count = 0 ; count < max ; count++ ) {
+			Finalizer.generate( count );
 		}
-		
-		System.out.println("Look at that heap of trash!");
+
+		System.out.println( "Look at that heap of trash!" );
 
 	}
 
--- srcc/com/training/chapter4/Help.java
+++ srcc/com/training/chapter4/Help.java
@@ -1,3 +1,4 @@
+
 package com.training.chapter4;
 
 import java.util.Arrays;
@@ -18,69 +19,62 @@ public class Help {
 	private static final List<String> choices;
 
 	static {
-		ifSyntax = Arrays.asList("The if:\n", "if(condition) statement'",
-				"else statement;");
-		forSyntax = Arrays.asList("The switch:\n",
-				"for(init; condition; iteration) statement;");
-		whileSyntax = Arrays.asList("The while:\n",
-				"while(condition) statement;");
-		breakSyntax = Arrays.asList("The while:\n", "break; or break label;");
-		continueSyntax = Arrays.asList("The continue:\n",
-				"continue; or continue label;");
-		doWhileSyntax = Arrays.asList("The do-while:\n", "do{", "  statement",
-				"while(condition);");
-		switchSyntax = Arrays.asList("The for:\n", "switch(expression) {",
-				"  case constant:", "    statment sequence", "    break",
-				"//...", "}");
-		invalidArg = Arrays.asList("\nInvalid character",
-				"Choose one of the below:");
-		exitStatement = Arrays.asList("\nChoose another option:");
-		choices = Arrays.asList("  1. if\n", "  2. switch\n", "  3. for\n",
-				"  4. while\n", "  5. do-while\n", "  6. break\n",
-				"  7. continue\n", "  q. exit\n", "Choose one:");
+		ifSyntax = Arrays.asList( "The if:\n", "if(condition) statement'",
+		    "else statement;" );
+		forSyntax = Arrays.asList( "The switch:\n",
+		    "for(init; condition; iteration) statement;" );
+		whileSyntax = Arrays.asList( "The while:\n",
+		    "while(condition) statement;" );
+		breakSyntax = Arrays.asList( "The while:\n", "break; or break label;" );
+		continueSyntax = Arrays.asList( "The continue:\n",
+		    "continue; or continue label;" );
+		doWhileSyntax = Arrays.asList( "The do-while:\n", "do{", "  statement",
+		    "while(condition);" );
+		switchSyntax = Arrays.asList( "The for:\n", "switch(expression) {",
+		    "  case constant:", "    statment sequence", "    break",
+		    "//...", "}" );
+		invalidArg = Arrays.asList( "\nInvalid character",
+		    "Choose one of the below:" );
+		exitStatement = Arrays.asList( "\nChoose another option:" );
+		choices = Arrays.asList( "  1. if\n", "  2. switch\n", "  3. for\n",
+		    "  4. while\n", "  5. do-while\n", "  6. break\n",
+		    "  7. continue\n", "  q. exit\n", "Choose one:" );
 	}
 
 	public static void printChoices() {
-		choices.stream().forEach(System.out::print);
+		choices.stream().forEach( System.out::print );
 	}
 
-	public static boolean printOption(char option) {
+	public static boolean printOption( char option ) {
 
-		switch (option) {
+		switch ( option ) {
 
-		case '1':
-			Stream.concat(ifSyntax.stream(), exitStatement.stream()).forEach(
-					System.out::println);
-			break;
-		case '2':
-			Stream.concat(switchSyntax.stream(), exitStatement.stream())
-					.forEach(System.out::println);
-			break;
-		case '3':
-			Stream.concat(forSyntax.stream(), exitStatement.stream()).forEach(
-					System.out::println);
-			break;
-		case '4':
-			Stream.concat(whileSyntax.stream(), exitStatement.stream())
-					.forEach(System.out::println);
-			break;
-		case '5':
-			Stream.concat(doWhileSyntax.stream(), exitStatement.stream())
-					.forEach(System.out::println);
-			break;
-		case '6':
-			Stream.concat(breakSyntax.stream(), exitStatement.stream())
-					.forEach(System.out::println);
-			break;
-		case '7':
-			Stream.concat(continueSyntax.stream(), exitStatement.stream())
-					.forEach(System.out::println);
-			break;
-		case 'q':
-			return false;
-		default:
-			invalidArg.stream().forEach(System.out::println);
-			break;
+			case '1':
+				Stream.concat( ifSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '2':
+				Stream.concat( switchSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '3':
+				Stream.concat( forSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '4':
+				Stream.concat( whileSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '5':
+				Stream.concat( doWhileSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '6':
+				Stream.concat( breakSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case '7':
+				Stream.concat( continueSyntax.stream(), exitStatement.stream() ).forEach( System.out::println );
+				break;
+			case 'q':
+				return false;
+			default:
+				invalidArg.stream().forEach( System.out::println );
+				break;
 		}
 
 		return true;
