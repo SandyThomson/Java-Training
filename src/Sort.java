1596e431752fe2b01454f6ed692ac64809ed8a58
Alex Deas <alex.deas@datacash.com>
2014-11-26 17:28:27 +0000

    Retabbing

--- src/Sort.java
+++ src/Sort.java
@@ -3,40 +3,40 @@
  */
 
 public class Sort {
-	
-	public static void main ( String[] args ) {
-		
-		// Set some strings
-		String[] strings = { "Ardbeg", "Cragganmore", "Glenmorangie", "Laphroaig", "Glenfarclas",
-												 "Jura", "Talisker", "Lavagulin", "Bowmore", "Glenlivet" };
-		
-		String t;
-		int a, b;
-		int size = strings.length;
+  
+  public static void main ( String[] args ) {
+    
+    // Set some strings
+    String[] strings = { "Ardbeg", "Cragganmore", "Glenmorangie", "Laphroaig", "Glenfarclas",
+                         "Jura", "Talisker", "Lavagulin", "Bowmore", "Glenlivet" };
+    
+    String t;
+    int a, b;
+    int size = strings.length;
 
-		// Display original array
-		System.out.print( "Original array is:");
-		for ( String string : strings) System.out.print ( " " + string );
-		System.out.println();
-		
-		// Bubbler
-		for ( a = 1; a < size ; a++ ) {
-			for ( b = size - 1 ; b >= a ; b -- )  {
-				
+    // Display original array
+    System.out.print( "Original array is:");
+    for ( String string : strings) System.out.print ( " " + string );
+    System.out.println();
+    
+    // Bubbler
+    for ( a = 1; a < size ; a++ ) {
+      for ( b = size - 1 ; b >= a ; b -- )  {
+        
         if ( strings[b - 1].compareTo(strings[b])> 0 ){
-        	// switch strings
-        	t = strings[b - 1];
-        	strings[b - 1] = strings[b];
-        	strings[b] = t;
-        	
+          // switch strings
+          t = strings[b - 1];
+          strings[b - 1] = strings[b];
+          strings[b] = t;
+          
         }
-			}
-		}
-		
-		// Display new array
-		System.out.print( "New array is:");
-		for ( String string : strings) System.out.print ( " " + string );
-		System.out.println();
-	
-	}
+      }
+    }
+    
+    // Display new array
+    System.out.print( "New array is:");
+    for ( String string : strings) System.out.print ( " " + string );
+    System.out.println();
+  
+  }
 }
