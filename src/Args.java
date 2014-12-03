19470f42cf6470f96ae98fad91a8f1e6c8c316ff
Alex Deas <alex.deas@datacash.com>
2014-12-03 16:00:07 +0000

    Chapter 6 exercises

---
+++ src/Args.java
@@ -0,0 +1,23 @@
+/*
+ * Demonstrating varargs
+ */
+
+public class Args {
+	
+	public static void main ( String[] args ) {
+		
+		int[] array = { 1, 2, 3, 4, 5 };
+		
+		System.out.println( sum( array ) );
+	}
+	
+	static int sum ( int ... array ) {
+		
+		int sum = 0;
+		
+		for ( int i : array ) sum += i;
+	
+		return sum;
+	}
+
+}
---
+++ src/StackDemo.java
@@ -0,0 +1,39 @@
+/*
+ * Try to use a stack
+ */
+
+
+public class StackDemo {
+	
+	public static void main ( String[] args ) {
+    Stack stack = new Stack( 5 );
+	
+    System.out.println( "Made a stack");
+    
+    System.out.println( stack.pop( ) );
+    
+    stack.push( 'a' );
+    stack.push( 'b' );
+    stack.push( 'c' );
+    stack.push( 'd' );
+    stack.push( 'e' );
+    
+    stack.push( 'a' );
+    
+    System.out.println( "To String: " + stack.toString() );
+    
+    System.out.println( stack.pop( ) );
+    System.out.println( "To String: " + stack.toString() );
+    
+    System.out.println( stack.pop( ) );
+    System.out.println( "To String: " + stack.toString() );
+    
+    stack.push( 'f' );
+    System.out.println( "To String: " + stack.toString() );
+    
+    System.out.println( stack.pop( ) );
+    System.out.println( "To String: " + stack.toString() );
+    
+	}
+
+}
