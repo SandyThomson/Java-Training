0477544f9cadd0e617e4349005cce224053d4c62
David Cox <dave@codavex.co.uk>
2014-12-17 16:41:17 +0000

    dcox chapter 8 exercises

---
+++ src/ch08/Vehicle/IVehicle.java
@@ -0,0 +1,23 @@
+package ch08.Vehicle;
+
+public interface IVehicle {
+
+	// Return the range.
+	int range();
+
+	// Compute fuel needed for a given distance.
+	double fuelneeded( int miles );
+
+	// Access methods for instance variables. 
+	int getPassengers();
+
+	void setPassengers( int p );
+
+	int getFuelcap();
+
+	void setFuelcap( int f );
+
+	int getMpg();
+
+	void setMpg( int m );
+}
