468c1b370ed090a58ba4fc5de1db484226711f1b
Simon Gross <spg@datacash.com>
2015-01-14 09:03:40 +0000

    Chapter 9

---
+++ src/com/datacash/training/ArrayEmptyException.java
@@ -0,0 +1,10 @@
+
+package com.datacash.training;
+
+public class ArrayEmptyException extends Exception {
+
+	public ArrayEmptyException( String string ) {
+		super( string );
+	}
+
+}
---
+++ src/com/datacash/training/ArrayFullException.java
@@ -0,0 +1,10 @@
+
+package com.datacash.training;
+
+public class ArrayFullException extends Exception {
+
+	public ArrayFullException( String string ) {
+		super( string );
+	}
+
+}
