e1c8937b111938c6c9242585981ffa9a1241fa8a
Alex Deas <alex.deas@datacash.com>
2015-02-11 16:25:59 +0000

    retabbing

--- src/Chapter13/EmptyStackException.java
+++ src/Chapter13/EmptyStackException.java
@@ -2,9 +2,9 @@ package Chapter13;
 
 
 public class EmptyStackException extends Exception {
-	
-	 public String toString() {
-		 return "You've emptied your stack.";
+  
+   public String toString() {
+     return "You've emptied your stack.";
    }
 
 }
--- src/Chapter13/Stackable.java
+++ src/Chapter13/Stackable.java
@@ -2,9 +2,9 @@ package Chapter13;
 // Stack interface with a less rubbish name
 
 public interface Stackable<T> {
-	
-	void push(T object) throws FullStackException;
-	
-	T pop() throws EmptyStackException;
+  
+  void push(T object) throws FullStackException;
+  
+  T pop() throws EmptyStackException;
 
 }
